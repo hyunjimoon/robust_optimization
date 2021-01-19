366f53e3f11af56725d85374f3600a8e6c1638d4
hyunjimoon <mhj1667@gmail.com>
2021-01-19 17:36:08 +0900

    ref update

--- interval_division/policy.py
+++ interval_division/policy.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
 import statsmodels.formula.api as smf
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import logging
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
+from sklearn import mixture
 
 #d: demand(pd.series)
 
@@ -19,6 +23,9 @@ def interval_div_q(d, p, c, s):
         return cost / len(se_I)
 
     L = int(int(d.shape[0]) * 0.3)  # TODO num_I = int(np.log(emp_dist.shape[0])) 등 실험
+    # with prior info on mu1, mu2, sd1, mu2, (w)
+    #se_I = 
+    # without prior info moderate data
     se_I = [pd.qcut(d, L, duplicates='drop').values[i] for i in range(L)]
 
     worst_cand = dict()
@@ -30,8 +37,6 @@ def interval_div_q(d, p, c, s):
         worst_cand[end] = E_cost_I(end, se_I, p, c, s)
 
     return min(worst_cand, key=worst_cand.get)
-
-
 
 def normal_ass_q(d, p, c, s):
     underage_cost = p - c
@@ -59,3 +64,66 @@ def dist_free_q(d, p, c, s):
     m = p / c - 1
     dd = 1 - s / c
     return Mean + (sigma / 2) * (np.sqrt(m / dd) - np.sqrt(dd / m))
+
+def fit_gmm_normalize_return_comp(df):
+    dfy = df.y.copy()
+    y = dfy #scaling todo (dfy - np.mean(dfy))/np.std(dfy) https://stackoverflow.com/questions/13161923/python-sklearn-mixture-gmm-not-robust-to-scale
+    X = np.expand_dims(y, 1)
+    N = np.arange(1, 8)
+    models = [None for i in range(len(N))]
+
+    for i in range(len(N)):
+        models[i] = GaussianMixture(N[i], covariance_type = 'spherical').fit(X)
+    AIC = [m.aic(X) for m in models]
+    BIC = [m.bic(X) for m in models]
+    fig = plt.figure(figsize=(18, 6))
+
+    # plot 1: data + best-fit mixture
+    ax = fig.add_subplot(131)
+    M_best = models[np.argmin(BIC)]
+
+    x = np.linspace(-6, 6, 1000)
+    logprob = M_best.score_samples(x.reshape(-1, 1))
+    responsibilities = M_best.predict_proba(x.reshape(-1, 1))
+    pdf = np.exp(logprob)
+    pdf_individual = responsibilities * pdf[:, np.newaxis]
+
+    ax.hist(X, 30, density=True, histtype='stepfilled', alpha=0.4)
+    ax.plot(x, pdf, '-k')
+    ax.plot(x, pdf_individual, '--k')
+    ax.text(0.04, 0.96, "Best-fit Mixture",
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$p(x)$')
+    print("best number of components", M_best.n_components)
+    
+    # plot 2: AIC and BIC
+    ax = fig.add_subplot(132)
+    ax.plot(N, AIC, '-k', label='AIC')
+    ax.plot(N, BIC, '--k', label='BIC')
+    ax.set_xlabel('n. components')
+    ax.set_ylabel('information criterion')
+    ax.legend(loc=2)
+
+    # plot 3: posterior probabilities for each component
+    ax = fig.add_subplot(133)
+
+    p = M_best.predict_proba(x.reshape(-1, 1))
+    #p = p[:, (1, 0, 2)]  # rearrange order so the plot looks better
+    p = p.cumsum(1).T
+
+    ax.fill_between(x, 0, p[0], color='gray', alpha=0.3)
+    ax.fill_between(x, p[0], p[1], color='gray', alpha=0.5)
+    ax.fill_between(x, p[1], 1, color='gray', alpha=0.7)
+    ax.set_xlim(-6, 6)
+    ax.set_ylim(0, 1)
+    ax.set_xlabel('$x$')
+    ax.set_ylabel(r'$p({\rm class}|x)$')
+
+
+    plt.show()
+    # print(M_best.weights_) #perfect
+    # print(M_best.means_) #perfect
+    # print(M_best.covariances_) #bad
+    # print((M_best.means_+np.mean(dfy)) * np.std(dfy))
+    return np.argmax(M_best.predict_proba(np.array(y).reshape(-1,1)), axis = 1)
--- interval_division/test.py
+++ interval_division/test.py
@@ -1,3 +1,101 @@
+# import itertools
+# from demand_generator import get_demand
+# import pandas as pd
+# import numpy as np
+# from matplotlib import pyplot as plt
+# from profit import profit, calc_beta_dist_model_profit
+# from sklearn.preprocessing import MinMaxScaler
+#
+# p = 10
+# # c = 20 determined by beta
+# s = 1
+# mu = 1000
+# sd = 1000 / 4
+# mu1s = np.arange(700, 900, 100).tolist()
+# n = 1000
+#
+# nana = list()
+# dfdf = list()
+# idid = list()
+# nana_p = list()
+# dfdf_p = list()
+# idid_p = list()
+#
+# ############
+# betas = [ 0.2, 0.5, 0.7, 0.8, 0.9]  #
+# models  = ['normal_ass', 'dist_free', 'interval_div']
+# model = 'interval_div'
+# ############
+#
+# demand_type = 'normal'
+# dist = get_demand(demand_type, mu, sd, n)
+# #dist = get_demand('mm', mu, sd, n, mu1=300, sd1=300 / 4, w1=.4)
+# # dist = get_demand('leather', 7920, 8405, 1)
+# pfs = [[m, calc_beta_dist_model_profit(betas, dist, m, p, s)] for m in models]
+# NA = list()
+# DF = list()
+# ID = list()
+# MM = list()
+# NA_n = list()
+# DF_n = list()
+# ID_n = list()
+# for i in range(0, len(betas)):
+#    NA = (pfs[0][1][i])
+#    DF = (pfs[1][1][i])
+#    ID = (pfs[2][1][i])
+#
+#    MM = (NA + DF + ID) / 3
+#    if MM == 0:
+#        MM = 1
+#    nana.append(NA)
+#    dfdf.append(DF)
+#    idid.append(ID)
+#
+#    NA_n.append(NA / MM)
+#    DF_n.append(DF / MM)
+#    ID_n.append(ID / MM)
+#
+#    nana_p.append(NA/MM)
+#    dfdf_p.append(DF/MM)
+#    idid_p.append(ID/MM)
+#
+# # print(np.mean(nana), np.mean(dfdf), np.mean(idid))
+# ## dataframe 형태로 csv 파일 저장 분산도 구해야
+# pd_nana_p = pd.DataFrame(nana_p)
+# pd_dfdf_p = pd.DataFrame(dfdf_p)
+# pd_idid_p = pd.DataFrame(idid_p)
+#
+# pd_nana = pd.DataFrame(nana)
+# pd_dfdf = pd.DataFrame(dfdf)
+# pd_idid = pd.DataFrame(idid)
+#
+# print('NA', round(np.mean(nana_p), 5), round(np.std(nana_p), 5), '   DF', round(np.mean(dfdf_p), 5), round(np.std(dfdf_p), 5), '   ID',round((np.mean(idid_p)), 5), round((np.std(idid_p)), 5))
+# print('NA', round(np.mean(nana), 5),round(np.std(nana), 5), '   DF', round(np.mean(dfdf), 5),round(np.std(dfdf), 5), '   ID',round((np.mean(idid)), 5), round((np.mean(idid)), 5))
+#
+# Filename = demand_type + '_mu_' + str(mu) + '_sd_' + str(int(sd))
+# pd_nana_p.to_csv(f"res/{Filename}_nana_p.csv", header = True, index = True)
+# pd_dfdf_p.to_csv(f"res/{Filename}_dfdf_p.csv", header = True, index = True)
+# pd_idid_p.to_csv(f"res/{Filename}_idid_p.csv", header = True, index = True)
+#
+# pd_nana.to_csv(f"res/{Filename}_nana.csv", header = True, index = True)
+# pd_dfdf.to_csv(f"res/{Filename}_dfdf.csv", header = True, index = True)
+# pd_idid.to_csv(f"res/{Filename}_idid.csv", header = True, index = True)
+#
+# fig, axes = plt.subplots(1,2, figsize =(12,6))
+# axes[0].hist(dist)
+#
+# axes[1].plot(betas, NA_n, label = "Normal assumption model")
+# axes[1].plot(betas, DF_n, label = "Distribution free model")
+# axes[1].plot(betas, ID_n, label = "Interval divide model")
+#
+# axes[1].set_xlabel("Beta")
+# axes[1].set_ylabel("ProfitRatio")
+# axes[1].set_ylim(0.95,1.05) #1 - 5*np.std(ID_n), 1 + 5*np.std(ID_n)) #
+# axes[1].set_title(Filename)
+# #fig.title(Filename)
+# fig.savefig(f'fig/{Filename}')
+# fig.show()
+
 import itertools
 from demand_generator import get_demand
 import pandas as pd
@@ -9,8 +107,8 @@ from sklearn.preprocessing import MinMaxScaler
 p = 10
 # c = 20 determined by beta
 s = 1
-mu = 1000
-sd = 1000 / 4
+mu = 180
+sd = 44
 mu1s = np.arange(700, 900, 100).tolist()
 n = 1000
 
@@ -24,74 +122,11 @@ idid_p = list()
 ############
 betas = [ 0.2, 0.5, 0.7, 0.8, 0.9]  #
 models  = ['normal_ass', 'dist_free', 'interval_div']
-model = 'interval_div'
+#model = 'interval_div'
 ############
 
-demand_type = 'gamma'
+demand_type = 'norm'
 dist = get_demand(demand_type, mu, sd, n)
 #dist = get_demand('mm', mu, sd, n, mu1=300, sd1=300 / 4, w1=.4)
 # dist = get_demand('leather', 7920, 8405, 1)
 pfs = [[m, calc_beta_dist_model_profit(betas, dist, m, p, s)] for m in models]
-NA = list()
-DF = list()
-ID = list()
-MM = list()
-NA_n = list()
-DF_n = list()
-ID_n = list()
-for i in range(0, len(betas)):
-   NA = (pfs[0][1][i])
-   DF = (pfs[1][1][i])
-   ID = (pfs[2][1][i])
-
-   MM = (NA + DF + ID) / 3
-   if MM == 0:
-       MM = 1
-   nana.append(NA)
-   dfdf.append(DF)
-   idid.append(ID)
-
-   NA_n.append(NA / MM)
-   DF_n.append(DF / MM)
-   ID_n.append(ID / MM)
-
-   nana_p.append(NA/MM)
-   dfdf_p.append(DF/MM)
-   idid_p.append(ID/MM)
-
-# print(np.mean(nana), np.mean(dfdf), np.mean(idid))
-## dataframe 형태로 csv 파일 저장 분산도 구해야
-pd_nana_p = pd.DataFrame(nana_p)
-pd_dfdf_p = pd.DataFrame(dfdf_p)
-pd_idid_p = pd.DataFrame(idid_p)
-
-pd_nana = pd.DataFrame(nana)
-pd_dfdf = pd.DataFrame(dfdf)
-pd_idid = pd.DataFrame(idid)
-
-print('NA', round(np.mean(nana_p), 5), round(np.std(nana_p), 5), '   DF', round(np.mean(dfdf_p), 5), round(np.std(dfdf_p), 5), '   ID',round((np.mean(idid_p)), 5), round((np.std(idid_p)), 5))
-print('NA', round(np.mean(nana), 5),round(np.std(nana), 5), '   DF', round(np.mean(dfdf), 5),round(np.std(dfdf), 5), '   ID',round((np.mean(idid)), 5), round((np.mean(idid)), 5))
-
-Filename = demand_type + '_mu_' + str(mu) + '_sd_' + str(int(sd))
-pd_nana_p.to_csv(f"res/{Filename}_nana_p.csv", header = True, index = True)
-pd_dfdf_p.to_csv(f"res/{Filename}_dfdf_p.csv", header = True, index = True)
-pd_idid_p.to_csv(f"res/{Filename}_idid_p.csv", header = True, index = True)
-
-pd_nana.to_csv(f"res/{Filename}_nana.csv", header = True, index = True)
-pd_dfdf.to_csv(f"res/{Filename}_dfdf.csv", header = True, index = True)
-pd_idid.to_csv(f"res/{Filename}_idid.csv", header = True, index = True)
-
-fig, axes = plt.subplots(1,2, figsize =(12,6))
-axes[0].hist(dist)
-
-axes[1].plot(betas, NA_n, label = "Normal assumption model")
-axes[1].plot(betas, DF_n, label = "Distribution free model")
-axes[1].plot(betas, ID_n, label = "Interval divide model")
-
-axes[1].set_xlabel("Beta")
-axes[1].set_ylabel("ProfitRatio")
-axes[1].set_ylim(0.95,1.05) #1 - 5*np.std(ID_n), 1 + 5*np.std(ID_n)) #
-axes[1].set_title(Filename)
-#fig.title(Filename)
-fig.savefig(f'fig/{Filename}')
-fig.show()
